3dcaca2fdebeb93041abd68bb00b3c56eb42acc8
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-08-21 20:39:06 +0300

    added blank TaskCrudDto class, to be implemented later

---
+++ src/main/java/edu/aptasinskij/jdbc/day2/dao/TaskCrudDao.java
@@ -0,0 +1,27 @@
+package edu.aptasinskij.jdbc.day2.dao;
+
+import edu.aptasinskij.jdbc.Task;
+
+public class TaskCrudDao implements CrudDao<Task, Long> {
+
+    @Override
+    public void create(Task entity) {
+
+    }
+
+    @Override
+    public Task select(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Task entity) {
+
+    }
+
+    @Override
+    public void delete(Task entity) {
+
+    }
+
+}
